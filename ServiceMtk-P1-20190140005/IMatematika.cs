552810937c493ef3b3d8c13ddd8ca3ab4df9a29b
ASUS <ASUS@LAPTOP-3882BD5J>
2021-10-05 12:40:02 -0700

    Add project files.

---
+++ ServiceMtk-P1-20190140005/IMatematika.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWCFService_005
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IMatematika" in both code and config file together.
+    [ServiceContract]
+    public interface IMatematika
+    {
+        [OperationContract]
+        /* output->*/
+        int Tambah(int a, int b); //method
+        [OperationContract]
+        int Kurang(int a, int b); //input
+        [OperationContract]
+        int Kali(int a, int b);
+        [OperationContract]
+        int Bagi(int a, int b);
+        [OperationContract]
+        Koordinat TKoordinat(Koordinat a, Koordinat b);
+    }
+    [DataContract]
+    public class Koordinat
+    {
+        private int _x, _y; //atribut
+        [DataMember]//membuka koordinat, x, y
+        public int X //properties
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        [DataMember]
+        public int Y //properties
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+    }
+}
---
+++ ServiceMtk-P1-20190140005/Matematika.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace FirstWCFService_005
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Matematika" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+       public int Bagi(int a, int b)
+        {
+            return a / b;
+        }
+        public int Kali(int a, int b)
+        {
+            return a * b;
+        }
+        public int Kurang(int a, int b)
+        {
+            return a - b;
+        }
+        public int Tambah(int a, int b)
+        {
+            return a + b;
+        }
+        //TKoordinat: nama methodnya, a, b= input seperti int
+        //Membuat koordinat hasil lalu menjumlahkan xa dengan xb, ya dengan yb lalu dimasukkan ke hasil
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            Koordinat hasil = new Koordinat();
+            hasil.X = a.X + b.X;
+            hasil.Y = a.Y + b.Y;
+            return hasil;
+        }
+    }
+}
